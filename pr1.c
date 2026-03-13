d1e285890182cb2da4f6e450a945281a9e81ad8f
Renish Korat <koratrenish350@gmail.com>
2026-03-13 12:07:16 +0530

    add

--- pr1.c
+++ pr1.c
@@ -7,16 +7,10 @@
      
     printf("Enter the bs :");
     scanf("%d", &bs);
-    printf("Enter the hra_per :");
-    scanf("%f", &hra_per);
-    printf("Enter the da_per :");
-    scanf("%f", &da_per);
-    printf("enter the ta_per :");
-    scanf("%f", &ta_per);
 
-    hra_amo  = bs * (hra_per / 100);
-    da_amo = bs * (da_per / 1000);
-    ta_amo = bs * (ta_per / 100);
+    hra_amo  = bs * (10 / 100);
+    da_amo = bs * (5 / 100);
+    ta_amo = bs * (8 / 100);
     g_salary = bs + hra_amo + da_amo + ta_amo;
 
     printf("The gross salary is : %.2f", g_salary);
